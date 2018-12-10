765597f92d1193908cded5634fa341ae61dcc9f9
Alexey Guilarte Noa <aguilartenoa@rccl.com>
2018-12-10 13:27:55 -0500

    Updated gradle dependency and fixed compilation errors.

--- app/src/main/java/shiptalk/com/shiptalk/ui/MainActivity.kt
+++ app/src/main/java/shiptalk/com/shiptalk/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 import shiptalk.com.shiptalk.R
 
